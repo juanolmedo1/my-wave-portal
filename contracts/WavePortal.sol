ebd9e0bb1ffad7fb3dd35b6d682a4bb70b076126
Juan Olmedo <juan.olmedo@eze-lmc-b03000.local>
2021-12-09 18:45:36 -0300

    Section 5: Done :white_check_mark:

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -3,27 +3,47 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract WavePortal {
+contract WavePortal is Ownable {
+
     event NewWave(address indexed from, uint256 timestamp, string message);
+    event NewWinner(address indexed from, uint256 timestamp, uint256 amount);
+
     struct Wave {
-        address waver; // The address of the user who waved.
-        string message; // The message the user sent.
-        uint256 timestamp; // The timestamp when the user waved.
+        address waver; 
+        string message;
+        uint256 timestamp;
     }
 
-    uint256 totalWaves;
-    Wave[] waves;
-    mapping(address => uint256) public countOfWaves;
+    uint256 public totalWaves;
+    uint256 private seed;
+    uint256 private cooldownTime = 30 seconds;
+    uint256 private winProbability = 40;
+    uint256 private priceAmount = 0.001 ether;
+    mapping(address => uint256) public lastWavedAt;
+    Wave[] public waves;
 
-    constructor() {
-        console.log("Yo yo, I am a contract and I am smart");
+    constructor() payable {
+        seed = (block.timestamp + block.difficulty) % 100;
+    }
+
+    function randomNumber() private view returns (uint256) {
+        return (block.difficulty + block.timestamp + seed) % 100;
     }
 
     function wave(string memory _message) public {
+        require(lastWavedAt[msg.sender] + cooldownTime < block.timestamp, "Wait 30 sec.");
         totalWaves++;
-        countOfWaves[msg.sender]++;
         waves.push(Wave(msg.sender, _message, block.timestamp));
+        seed = randomNumber();
+        lastWavedAt[msg.sender] = block.timestamp;
+        if (seed <= winProbability) {
+            require(priceAmount <= address(this).balance, "Trying to withdraw more money than the contract has.");
+            (bool success, ) = (msg.sender).call{value: priceAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+            emit NewWinner(msg.sender, block.timestamp, priceAmount);
+        }
         emit NewWave(msg.sender, block.timestamp, _message);
     }
 
@@ -33,5 +53,17 @@ contract WavePortal {
 
     function getTotalWaves() public view returns (uint256) {
         return totalWaves;
+    }
+
+    function updateCooldownTime(uint256 _newCooldownTime) public onlyOwner {
+        cooldownTime = _newCooldownTime;
+    }
+    
+    function updateWinProbability(uint256 _newWinProbability) public onlyOwner {
+        winProbability = _newWinProbability;
+    }
+
+    function updatePriceAmount(uint256 _newPriceAmount) public onlyOwner {
+        priceAmount = _newPriceAmount;
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,19 @@
 require("@nomiclabs/hardhat-waffle");
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+require('dotenv').config();
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
+const { STAGING_INFURA_KEY, PRIVATE_KEY } = process.env;
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: STAGING_INFURA_KEY,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
